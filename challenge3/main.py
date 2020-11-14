cc6a9c685a0d5362bedc6eacf89519e9b6e52cdb
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-14 14:49:44 +0100

    Refactor Challenge 3

--- challenge3/main.py
+++ challenge3/main.py
@@ -1,7 +1,36 @@
+"""
+Nel tempo libero, i membri del team tech si divertono ad inventare possibili nuove convenzioni per gli ID delle tabelle del database.
+L'ultima è la seguente:
+Un id è valido solo se
 
-def fun(stringa):
+1. ogni lettera appare lo stesso numero di volte.
+2. oppure rimuovendo un solo carattere, si ottiene una stringa che soddisfa la condizione 1
+
+Dato un possibile `id`, verifica se questo rispetta le specifiche citate.
+
+Input Format
+
+La prima riga contiene una stringa, l' `id` di cui bisogna verificare la validità
+
+Constraints
+
+*   1 <= |id| <= 10^5
+
+*   Ogni carattere appartiene all'insieme [a-z]
+
+Output Format
+
+Fornire
+
+*   "valido" se l `id` soddisfa le condizioni
+*   "non valido" altrimenti
+
+"""
+
+
+def fun(id):
     letters = dict()
-    for char in stringa:
+    for char in id:
         if char in letters:
             letters[char] += 1
         else:
@@ -19,5 +48,5 @@ def fun(stringa):
         return "non valido"
 
 
-stringa = str(input())
-print(fun(stringa))
+id = str(input())
+print(fun(id))
