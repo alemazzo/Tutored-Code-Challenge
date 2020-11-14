ace92377a21a70eb5d1e60a0e95650e39d6700b3
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-14 14:30:31 +0100

    Refactor Challenge 1

--- challenge1/main.py
+++ challenge1/main.py
@@ -1,16 +1,37 @@
+"""
+Tutored vuole acquistare dei nuovi crediti per Google Ads, che verranno usati nelle campagne pubblicitarie invernali. Si vuole sfruttare questo periodo per usufruire degli sconti che sono offerti ai clienti secondo la seguente politica:
+Dopo l'acquisto di un pacchetto di crediti, il successivo costa `s` euro in meno. Si può arrivare fino ad un costo minimo di `m`.
+Sapendo che il budget è di `b` euro, ed il costo di partenza è di `c`, trovare il numero massimo di pacchetti acquistabili.
 
-def fun(costo, sconto, minimo, fondi):
-    numPacchetti = 0
-    while fondi > 0:
+Input Format
+
+La prima riga contiene quattro interi,
+
+c = il costo iniziale
+s = lo sconto ad ogni acquisto
+m = il prezzo minimo raggiungibile
+b = il budget
+
+Constraints
+
+Output Format
+
+La funzione deve fornire un intero, rappresentante il massimo numero di pacchetti acquistabili.
+"""
+
+
+def fun(c, s, m, b):
+    packNum = 0
+    while b > 0:
         # compro
-        if costo > fondi:
+        if c > b:
             break
-        numPacchetti += 1
-        fondi -= costo
-        costo -= sconto
-        if costo < minimo:
-            costo = minimo
-    return numPacchetti
+        packNum += 1
+        b -= c
+        c -= s
+        if c < m:
+            c = m
+    return packNum
 
 
 c, s, m, b = str(input()).split()
