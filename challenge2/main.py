d2f15ba55184f4d990ac1ac843c232bef88ae902
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-14 14:45:00 +0100

    Refactor Challenge 2

--- challenge2/main.py
+++ challenge2/main.py
@@ -1,3 +1,30 @@
+"""
+Il team di marketing di Tutored ha finito di testare diverse campagne social. È arrivato il momento di analizzarne i risultati.
+Data una campagna, durata `n` giorni, sono stati registrate le variazioni di utenti in quei giorni.
+Sviluppa un algoritmo che date queste variazioni, restituisca la percentuale di giorni in cui c'è stato un incremento, un decremento e nessuna variazione.
+Se necessario la percentuale deve essere arrotondata alla prima cifra decimale.
+
+Input Format
+
+    La prima riga contiene un intero `n`, la durata in giorni della campagna
+    La seconda riga contiene il vettore `giorni`, contenente gli incremeneti di utenti durante la campagna
+
+Constraints
+
+* 1 < n < 100
+* -100 <= giorni[i] <= 100
+
+Output Format
+
+Stampa tre righe:
+
+    Nella prima la percentuale di giorni con incremento di utenti
+    Nella seconda la percentuale di giorni con decremento di utenti
+    Nella terza la percentuale di giorni con utenti invariati
+
+Se necessario, la percentuale deve essere arrotondata alla prima cifra decimale.
+"""
+
 
 def fun(days):
     reductions = 0
